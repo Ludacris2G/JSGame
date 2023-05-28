3d550b1456b57a2205aaf466435913411036d512
Liviu <liviutoma946@gmail.com>
2023-05-28 19:47:46 +0300

    added explosion canvas

--- index.js
+++ index.js
@@ -418,35 +418,54 @@ class Explosion {
         this.spriteHeight = 179;
         this.width = this.spriteWidth*0.5;
         this.height = this.spriteHeight*0.5;
-        this.x = x - this.width*0.5;
-        this.y = y - this.height*0.5;
+        this.x = x;
+        this.y = y;
         this.frame = 0;
         this.timer = 0;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = './sounds/boom.wav';
     }
     update() {
+        if (this.frame === 0) this.sound.play();
         this.timer++;
         if (this.timer % 15 === 0) {
             this.frame++;
         }
     }
     draw() {
-        ctx7.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        ctx7.save();
+        ctx7.translate(this.x, this.y);
+        ctx7.rotate(this.angle);
+        ctx7.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 0 - this.width*0.5, 0 - this.height*0.5, this.width, this.height)
         // ctx7.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh);
+        ctx7.restore();
     }
 }
     
 window.addEventListener('click', function(e) {
+    createAnimation(e);
+})
+
+// window.addEventListener('mousemove', (e) => {
+//     createAnimation(e);
+// })
+
+function createAnimation(e) {
     let positionX = e.x - canvasPosition.left;
     let positionY = e.y - canvasPosition.top;
     explosions.push(new Explosion(positionX, positionY));
-    console.log(explosions);
-})
+}
 
 function animate7() {
     ctx7.clearRect(0, 0, canvas7.width, canvas7.height);
     for (let i = 0; i < explosions.length; i++) {
         explosions[i].update();
         explosions[i].draw();
+        if (explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
     }
     requestAnimationFrame(animate7);
 }
